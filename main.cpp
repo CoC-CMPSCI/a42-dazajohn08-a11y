cacb2f1ae5ef9c623b244a284bf0e3a9b870ab5d
John <dazajohn08@gmail.com>
2026-03-24 22:33:41 -0700

    done 9

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+
+//?  [Problem Definition]
+//*     Write a program that asks for the weight of the package and
+//*     the distance it is to be shipped, and then displays the charges.
+//*
+//*  The Fast Freight Shipping Company charges the following rates:
+//*     Weight of Package (in Kilograms)          Rate per 500 Miles Shipped
+//*        2 kg or less                              $1.10
+//*        Over 2 kg but not more than 6 kg          $2.20
+//*        Over 6 kg but not more than 10 kg         $3.70
+//*        Over 10 kg but not more than 20 kg        $4.80
+//*
+//** Input Validation :
+//* 	Do not accept values of 0 or less for the weight of the package.
+//* 	Do not accept weights of more than 20 kg (this is the maximum weight the company will ship).
+//* 	Do not accept distances of less than 10 miles or more than 3,000 miles.
+//*     These are the company's minimum and maximum shipping distances.
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -13,16 +31,17 @@ int main()
     cout << "Enter the package weight and distance\n";
     cin >> weight >> distance;
 
+    // TODO: validate input - weight must be > 0 and <= 20
     if (weight <= 0 || weight > 20) {
-        cout << "Error: weight must be > 0 and <= 20." << endl;
-        return 1;
+    cout << "Error: weight must be > 0 and <= 20." << endl;
+    return 0;
     }
-
+    // TODO: validate input - distance must be >= 10 and <= 3000
     if (distance < 10 || distance > 3000) {
-        cout << "Error: distance must be >= 10 and <= 3000." << endl;
-        return 1;
+    cout << "Error: distance must be >= 10 and <= 3000." << endl;
+    return 0;
     }
-
+    // TODO: determine rate based on weight
     if (weight <= 2) {
         dRate = RATE2;
     } else if (weight <= 6) {
@@ -32,13 +51,12 @@ int main()
     } else {
         dRate = RATE20;
     }
-
+    // TODO: calculate price based on distance
     if (distance > 500) {
         price = (distance / 500.0) * dRate;
     } else {
         price = dRate;
     }
-
     // Use the following statements to print output
     cout << setprecision(2) << fixed;
     cout << "The shipping price for package is " << price << endl;
