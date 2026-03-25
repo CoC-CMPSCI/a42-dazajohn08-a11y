3166ee0033bb0fb02d8130b1f00fe38d4fedc398
John <dazajohn08@gmail.com>
2026-03-24 22:44:57 -0700

    bro idk

--- main.cpp
+++ main.cpp
@@ -8,34 +8,35 @@ int main()
     const double RATE6  = 2.20;
     const double RATE10 = 3.70;
     const double RATE20 = 4.80;
-
     double weight, distance, dRate, price;
 
+    cout << "Enter the package weight and distance\n";
     cin >> weight >> distance;
 
-    if (weight <= 0 || weight > 20)
-    {
-        cout << "Invalid input" << endl;
-        return 1;
+    if (weight <= 0 || weight > 20) {
+        cout << "Error: weight must be > 0 and <= 20." << endl;
+        return 0; 
+
+    if (distance < 10 || distance > 3000) {
+        cout << "Error: distance must be >= 10 and <= 3000." << endl;
+        return 0;
     }
 
-    if (distance < 10 || distance > 3000)
-    {
-        cout << "Invalid input" << endl;
-        return 1;
-    }
-
-    if (weight <= 2)
+    if (weight <= 2) {
         dRate = RATE2;
-    else if (weight <= 6)
+    } else if (weight <= 6) {
         dRate = RATE6;
-    else if (weight <= 10)
+    } else if (weight <= 10) {
         dRate = RATE10;
-    else
+    } else {
         dRate = RATE20;
+    }
 
-    int segments = (distance + 499) / 500;
-    price = segments * dRate;
+    if (distance > 500) {
+        price = (distance / 500.0) * dRate;
+    } else {
+        price = dRate;
+    }
 
     cout << setprecision(2) << fixed;
     cout << "The shipping price for package is " << price << endl;
